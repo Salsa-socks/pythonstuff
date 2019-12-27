2b649ff69f428baab3299dc1d072a8e4b1251f39
bnkosi2019 <47521522+bnkosi2019@users.noreply.github.com>
2019-12-27 22:18:04 +0200

    Client error on Mac, fine on PC

---
+++ pygame/server.py
@@ -0,0 +1,17 @@
+import socket
+import thread
+import sys
+
+server = ""
+port = 5555
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server.port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for a connection, Server started")
+
+#server above - initialize
